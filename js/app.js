e63b68cb51129718a4e892540e14ac4095a0b881
David Becher <david@skookum.com>
2013-03-06 10:56:02 -0500

    add messages model/collection, add messages to list on reset and when one is added

--- js/app.js
+++ js/app.js
@@ -5,22 +5,41 @@
   });
 
   var ChatList = BaseView.extend({
+    collection: new MessageCollection(),
+
     title: 'Chat',
     rightBarButtons: [
       { html: '<button class="barButton">Settings</button>', click: 'settings' }
     ],
 
+    events: {
+      //"click .message": 'goToMessage'
+    },
+
     initialize: function() {
     	this.render();
+    	this.listenTo(this.collection, 'reset', this.render);
+    	this.listenTo(this.collection, 'add', this.addMessage);
     },
 
     render: function() {
-    	this.$el.html('<h1>Chat!</h1>');
+      this.$el.empty();
+      if(this.collection.length === 0) {
+        return this.$el.html('<h2 class="no-messages">No Messages</h2>');
+      }
+      var ul = $('<ul class="chat-messages"></ul>');
+      this.collection.each(function(message) {
+        ul.append('<li class="chat-message"><p class="chat-message-text">' + message.get('message') + '</p><h3 class="chat-message-name">' + message.get('user') + '</h3></li>');
+      }, this);
+
+      this.$el.append(ul);
     },
 
-    settings: function() {
+    addMessage: function(message) {
+      this.$('.chat-messages').append('<li class="chat-message"><p class="chat-message-text">' + message.get('message') + '</p><h3 class="chat-message-name">' + message.get('user') + '</h3></li>')
+    },
 
-    }
+    settings: function() {}
   });
 
   RootView.pushView( new ChatList() );
---
+++ js/model.js
@@ -0,0 +1,45 @@
+;(function(exports) {
+
+  var MessageModel = Backbone.Model.extend({});
+
+  var MessageCollection = Backbone.Collection.extend({
+    model: MessageModel,
+
+    initialize: function() {
+      // initialize chat streaming here
+      var self = this;
+      window.setInterval(function() {
+        self.onMessage({ user: 'David', message: 'Another One!' });
+      }, 5000);
+      this.fetchFromLocalStorage();
+      if(this.length == 0) {
+        _.each([
+          { user: 'David', message: 'Hello' },
+          { user: 'Jim', message: 'Hi there' }
+        ], this.onMessage, this);
+      }
+    },
+
+    onMessage: function(message) {
+      this.push(message);
+      this.updateLocalStorage();
+    },
+
+    // update local chat history with at most the last 100 messages
+    updateLocalStorage: function() {
+      window.localStorage.setItem(
+        'chatHistory',
+        JSON.stringify( _.first( this.toJSON(), 100 ) )
+      );
+    },
+    // fetch chat history from localstorage
+    fetchFromLocalStorage: function() {
+      var messages = JSON.parse(window.localStorage.getItem('chatHistory'));
+      if(messages && messages.length) this.reset(messages);
+    }
+
+  });
+
+  exports.MessageCollection = MessageCollection;
+
+})(this);
