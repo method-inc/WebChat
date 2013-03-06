7ae721647a0a368a5fdebd3c33552cf6eb7da550
David Becher <david@skookum.com>
2013-03-06 11:57:59 -0500

    add template compiler

---
+++ js/templates.js
@@ -0,0 +1,10 @@
+;(function(exports) { 
+   exports.Templates = exports.Templates || {}; 
+   var Templates = exports.Templates; 
+   Templates["navigation"] = function(data){
+var __t,__p='',__j=Array.prototype.join,print=function(){__p+=__j.call(arguments,'');};
+__p+='<div class="navigation-title">\n  <h1 class="navigation-title-text">Navigation Title</h1>\n  <div class="barButtons left"></div>\n  <div class="barButtons right"></div>\n</div>\n<div class="navigation-content"></div>';
+return __p;
+}
+
+   })(this);
